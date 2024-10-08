27804cf0eb7e0958de84eea0e736a7bc358c76e4
John Machan <55124115+cb-machan@users.noreply.github.com>
2024-10-08 23:00:24 +0530

    DOCS-6609: Reverted port number changes.

--- payment-component/vanilla-js/apps/client/server.js
+++ payment-component/vanilla-js/apps/client/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 8084;
+const PORT = process.env.PORT || 8081;
 
 const server = http.createServer((req, res) => {
     let filePath = '.' + req.url;
